67875c914e299e3d93bfa03f8776a24d6a3cf698
juan <juanbadaldesportes@gmail.com>
2023-11-28 16:07:05 +0100

    Improved lab and finished master cleaning function

--- functions.py
+++ functions.py
@@ -21,7 +21,87 @@ def st_to_state(df: pd.DataFrame) -> pd.DataFrame:
         return df
     else:
         'st column not found in the dataframe'
- 
+
+        
+        
+def remove_null_rows(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Drops all rows from a dataframe in which all columns have null values.
+    '''
+    df.dropna(how='all', inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+
+def remove_duplicates(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Removes fully duplicated rows from a dataframe and resets index.
+    '''
+    df.drop_duplicates(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+
+def remove_nulls_col(df: pd.DataFrame, colname='gender') -> pd.DataFrame:
+    '''
+    Drops all rows from a dataframe where the value of a specific column (default='gender') is null.
+    '''
+    df.dropna(subset=[colname], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+
+def fix_percentages(df: pd.DataFrame, colname='customer_lifetime_value') -> pd.DataFrame:
+    '''
+    Process values in a DataFrame column:
+    - If the value is NaN, do nothing.
+    - If the value contains '%', change to blank, convert to float, and divide by 100.
+    - If it does not contain '%', convert to float.
+    
+    Parameters:
+    - df: pandas DataFrame.
+    - column_name: Name of the column to process.
+    
+    Returns:
+    - processed_df: DataFrame with the processed values.
+    '''
+    def process_value(value):
+        if pd.isna(value):
+            return value
+        elif '%' in str(value):
+            return float(str(value).replace('%', '')) / 100
+        else:
+            return float(value)
+
+    df[colname] = df[colname].apply(process_value)
+    
+    return df
+
+
+def impute_nulls(df: pd.DataFrame, colname='customer_lifetime_value', method='median') -> pd.DataFrame:
+    '''
+    Imputes statistic value (default=median) for nulls values in a column (default='customer_lifetime_value').
+    '''
+    if colname not in df.columns:
+        print(f"Column '{colname}' not found in the DataFrame.")
+        return df
+
+    if method == 'mean':
+        fill_value = df[colname].mean()
+    elif method == 'median':
+        fill_value = df[colname].median()
+    elif method == 'mode':
+        fill_value = df[colname].mode().iloc[0]
+    else:
+        print("Invalid method. Defaulting to median.")
+        fill_value = df[colname].median()
+
+    df[colname].fillna(fill_value, inplace=True)
+    return df
+
 
 
 def standardize_gender_col(df: pd.DataFrame) -> pd.DataFrame:
@@ -56,19 +136,10 @@ def bachelors_to_bachelor(df: pd.DataFrame, options={'Bachelors': 'Bachelor'}) -
         df['education'] = df['education'].replace(options)
         return df
     else:
-        print('education column not found in the dataframe')
+        print('education column not found in the dataframe')        
 
         
-        
-def fix_percentages(df: pd.DataFrame, colname='customer_lifetime_value') -> pd.DataFrame:
-    '''
-    Takes a dataframe column (default='customer_lifetime_value'), removes percentage symbol, converts to float and divides by 100.
-    '''
-    df[colname] = df[colname].replace('%', '', regex=True).astype(float) / 100
-    return df
-
-
-
+               
 def standardize_vehicle_class(df: pd.DataFrame, options={'Sports Car': 'Luxury', 'Luxury SUV': 'Luxury', 'Luxury Car': 'Luxury'}) -> pd.DataFrame:
     '''
     If the dataframe has a 'vehicle_class' column, replaces values in the column using dict mapping.
@@ -80,65 +151,29 @@ def standardize_vehicle_class(df: pd.DataFrame, options={'Sports Car': 'Luxury',
         print('vehicle_class column not found in the dataframe')
         
         
-        
+                
 def fix_complaints_column(df: pd.DataFrame, colname='number_of_open_complaints') -> pd.DataFrame:
     '''
-    Takes a dataframe column (default='number_of_open_complaints'), splits it by forwardslash, takes the middle value and converts to float.
+    - Takes a dataframe column (default='number_of_open_complaints')
+    - If length of the column value is 1, leaves it as is
+    - Else, splits the string by '/' and keeps the second value 
     '''
-    df[colname] = df[colname].str.split(pat='/').str[1].astype(float)
+    def process_string(value):
+        if len(value) == 1:
+            return int(value)
+        else:
+            return int(value.split('/')[1])
+    df[colname] = df[colname].apply(process_string)
     return df
 
 
 
-def remove_null_rows(df: pd.DataFrame) -> pd.DataFrame:
+def column_to_int(df: pd.DataFrame, colname='income') -> pd.DataFrame:
     '''
-    Drops all rows from a dataframe in which all columns have null values.
+    Takes a dataframe column(default='income') and casts it to integer.
     '''
-    df.dropna(how='all', inplace=True)
-    df.reset_index(drop=True, inplace=True)
+    df[colname] = df[colname].astype(int)
     return df
-
-
-
-def remove_nulls_col(df: pd.DataFrame, colname='gender') -> pd.DataFrame:
-    '''
-    Drops all rows from a dataframe where the value of a specific column (default='gender') is null.
-    '''
-    df.dropna(subset=colname, inplace=True)
-    return df
-
-
-
-def impute_nulls(df: pd.DataFrame, colname='customer_lifetime_value', method='median') -> pd.DataFrame:
-    '''
-    Imputes statistic value (default=median) for nulls values in a column (default='customer_lifetime_value').
-    '''
-    if colname not in df.columns:
-        print(f"Column '{colname}' not found in the DataFrame.")
-        return df
-
-    if method == 'mean':
-        fill_value = df[colname].mean()
-    elif method == 'median':
-        fill_value = df[colname].median()
-    elif method == 'mode':
-        fill_value = df[colname].mode().iloc[0]
-    else:
-        print("Invalid method. Defaulting to median.")
-        fill_value = df[colname].median()
-
-    df[colname].fillna(fill_value, inplace=True)
-    return df
-
-
-def remove_duplicates(df: pd.DataFrame) -> pd.DataFrame:
-    '''
-    Removes fully duplicated rows from a dataframe and resets index.
-    '''
-    df.drop_duplicates(inplace=True)
-    df.reset_index(drop=True, inplace=True)
-    return df
-
 
 
 def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
@@ -147,14 +182,15 @@ def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     '''
     clean_column_names(df)
     st_to_state(df)
+    remove_null_rows(df)
+    remove_duplicates(df)
+    remove_nulls_col(df)
+    fix_percentages(df)
+    impute_nulls(df)
     standardize_gender_col(df)
     standardize_state_col(df)
     bachelors_to_bachelor(df)
-    fix_percentages(df)
     standardize_vehicle_class(df)
     fix_complaints_column(df)
-    remove_null_rows(df)
-    remove_nulls_col(df)
-    impute_nulls(df)
-    remove_duplicates(df)
+    column_to_int(df)
     return df
